bd39bb47f5b239e02d5f1513e85eb340637d0023
Coru89 <corey.james.keller@gmail.comcom>
2023-03-17 22:38:02 +0700

    calendar bugs

--- source/scripts/calendar.ts
+++ source/scripts/calendar.ts
@@ -1,22 +1,55 @@
 // import * as Bootstrap from 'bootstrap'
-import { Popover } from 'bootstrap';
+import { Popover } from "bootstrap";
 
 export namespace Calendar {
   const els = document.querySelectorAll('[data-toggle="popover"]');
-  
+
   els.forEach((el) => {
-    const titleEl = el.querySelector('.qtip .title');
-    const contentEl = el.querySelector('.qtip .content');
+    const titleEl = el.querySelector(".qtip .title");
+    const contentEl = el.querySelector(".qtip .content");
 
     new Popover(el, {
-      title: titleEl ? titleEl.outerHTML : 'error',
-      content: contentEl ? contentEl.outerHTML : 'Could not retrieve event',
-      trigger: 'hover focus',
+      title: titleEl ? titleEl.outerHTML : "error",
+      content: contentEl ? contentEl.outerHTML : "Could not retrieve event",
+      trigger: "hover focus",
       html: true,
-      placement: 'top'
-    }); 
+      placement: "top",
+    });
   });
+
+  let myIframe: HTMLIFrameElement | null = document.querySelector("#calendar");
+
+  if (myIframe) {
+    myIframe.addEventListener("load", function () {
+      if (myIframe && myIframe.contentWindow) {
+        const height = myIframe.contentWindow.document.body.scrollHeight;
+        // Set the iframe's height to the calculated height
+        myIframe.style.height = `${height}px`;
+      }
+    });
+  }
+
+  window.addEventListener("resize", resizeIframe);
+
+  // Define the function that resizes the iframe
+  function resizeIframe() {
+    // Get the height of the iframe's content
+    if (myIframe && myIframe.contentWindow) {
+      const height = myIframe.contentWindow.document.body.scrollHeight;
+
+      // Set the iframe's height to the calculated height
+      myIframe.style.height = `${height}px`;
+    }
+  }
+
+  // let heightVal;
+
+  // {# myIframe.addEventListener("load", function() {
+  //   window.addEventListener('message', function(e) {
+  //     var eventName = e.data[0];
+  //     heightVal = e.data[1];
+  //     console.log(heightVal);
+  //     myIframe.style.height = heightVal + 'px';
+  //   }, false);
+  // }); #}
 }
-
-
-
