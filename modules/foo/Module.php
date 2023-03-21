7877b9456dc349dc6575a6767ca8b4f81124d3da
Coru89 <corey.james.keller@gmail.comcom>
2023-03-22 02:35:14 +0700

    form

--- modules/foo/Module.php
+++ modules/foo/Module.php
@@ -21,40 +21,52 @@ class Module extends \yii\base\Module
                 /** @var Submission $submission */
                 $submission = $e->sender;
 
-                // Make sure that `message[Phone]` was filled in
+                // if (empty($submission->message['body'])) {
+                //     // Add the error
+                //     // (This will be accessible via `message.getErrors(fromName')` in the template.)
+                //     $submission->addError('message.body', 'Comments cannot be blank.');
+                // }
+
+                // if (empty($submission->fromEmail)) {
+                //     // Add the error
+                //     // (This will be accessible via `message.getErrors(fromName')` in the template.)
+                //     $submission->addError('fromEmail', 'Email cannot be blank.');
+                // }
+
+                // Make sure that `fromName` was filled in
                 if (empty($submission->fromName)) {
                     // Add the error
-                    // (This will be accessible via `message.getErrors('message.phone')` in the template.)
-                    $submission->addError('fromName', 'Hello World.');
+                    // (This will be accessible via `message.getErrors(fromName')` in the template.)
+                    $submission->addError('fromName', 'Name cannot be blank.');
                 }
 
-            // Make sure that `message[Phone]` was filled in
+                // Make sure that `message[Company Name]` was filled in
                  if (empty($submission->message['Company Name'])) {
                      // Add the error
-                     // (This will be accessible via `message.getErrors('message.phone')` in the template.)
+                     // (This will be accessible via `message.getErrors('message.companyName')` in the template.)
                      $submission->addError('message.companyName', 'Company name cannot be blank.');
                  }
 
-                // // Make sure that `message[Phone]` was filled in
-                // if (empty($submission->message['dateRequested'])) {
-                //     // Add the error
-                //     // (This will be accessible via `message.getErrors('message.phone')` in the template.)
-                //     $submission->addError('message.dateRequested', 'Date Requested cannot be blank.');
-                // }
+                 // Make sure that `message[Phone]` was filled in
+                 if (empty($submission->message['Date Requested'])) {
+                     // Add the error
+                     // (This will be accessible via `message.getErrors('message.phone')` in the template.)
+                     $submission->addError('message.dateRequested', 'Date time cannot be blank.');
+                 }
 
-                // // Make sure that `message[Phone]` was filled in
-                // if (empty($submission->message['startTime'])) {
-                //     // Add the error
-                //     // (This will be accessible via `message.getErrors('message.phone')` in the template.)
-                //     $submission->addError('message.startTime', 'Hello World.');
-                // }
+                 // Make sure that `message[Phone]` was filled in
+                 if (empty($submission->message['Start Time'])) {
+                     // Add the error
+                     // (This will be accessible via `message.getErrors('message.phone')` in the template.)
+                     $submission->addError('message.startTime', 'Start time cannot be blank.');
+                 }
 
-                // // Make sure that `message[Phone]` was filled in
-                // if (empty($submission->message['endTIme'])) {
-                //     // Add the error
-                //     // (This will be accessible via `message.getErrors('message.phone')` in the template.)
-                //     $submission->addError('message.endTime', 'Hello World.');
-                // }
+                 // Make sure that `message[Phone]` was filled in
+                 if (empty($submission->message['End Time'])) {
+                     // Add the error
+                     // (This will be accessible via `message.getErrors('message.phone')` in the template.)
+                     $submission->addError('message.endTime', 'End time cannot be blank.');
+                 }
             }
         );
     }
