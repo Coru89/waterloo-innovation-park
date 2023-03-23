d47db70fd28b200cece6b0508687981b9c06825b
Coru89 <corey.james.keller@gmail.comcom>
2023-03-23 22:47:05 +0700

    slider

--- source/scripts/slider.ts
+++ source/scripts/slider.ts
@@ -1,31 +1,40 @@
 import Glide from'@glidejs/glide';
 
 export namespace slider {
-    window.addEventListener('DOMContentLoaded', () => {
-        var glide = new Glide('#glide', {
+    const glideEl = document.querySelector('.glide');
+        var glide = new Glide('.glide', {
         type: 'carousel',
         autoplay: 10000,
         gap: 30,
         perView: 1,
         peek: {
-            before: 180,
-            after: 180
+            before: 350,
+            after: 350
         },
         breakpoints: {
-            750: {
-            peek: {
-                before: 0,
-                after: 0
-            }
+            768: {
+                peek: {
+                    before: 0,
+                    after: 0
+                }
             },
-            990: {
-            peek: {
-                before: 130,
-                after: 130
-            }
+            1440: {
+                peek: {
+                    before: 130,
+                    after: 130
+                }
             }
         }
-        }).mount();
-    });
+        })      
+
+        // fixes cumulative layout shift
+        glide.on(['build.before'], function() {
+            if (glideEl) {
+                glideEl.classList.add('image-slider--visible');
+            }
+          });
+
+          glide.mount()
+
 }
     
