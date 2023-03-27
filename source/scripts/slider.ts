e06a1087eff5cb6d1190056db7b63537f308ba9a
Coru89 <corey.james.keller@gmail.comcom>
2023-03-28 02:24:39 +0700

    event slider

--- source/scripts/slider.ts
+++ source/scripts/slider.ts
@@ -2,17 +2,22 @@ import Glide from'@glidejs/glide';
 
 export namespace slider {
     const glideEl = document.querySelector('.glide');
+    const glideEventEl = document.querySelector('.glide-event');
     const glideInstaEl = document.querySelector('.instagram-glide');
 
     if (glideEl) {
         initImageGlide();
     }
 
+    if (glideEventEl) {
+        initEventGlide();
+    }
+
     if (glideInstaEl) {
         initInstaGlide();
     }
 
-
+    // image slider
     function initImageGlide() {
         var glide = new Glide('.glide', {
             type: 'carousel',
@@ -20,8 +25,71 @@ export namespace slider {
             gap: 30,
             perView: 1,
             peek: {
-                before: 350,
-                after: 350
+                before: 420,
+                after: 420
+            },
+            breakpoints: {
+                768: {
+                    peek: {
+                        before: 0,
+                        after: 0
+                    }
+                },
+                1440: {
+                    peek: {
+                        before: 130,
+                        after: 130
+                    }
+                }
+            }
+            })      
+    
+            // fixes cumulative layout shift
+            glide.on(['build.before'], function() {
+                if (glideEl) {
+                    glideEl.classList.add('image-slider--visible');
+                }
+              });
+
+    
+              glide.mount()
+    }
+
+    // instagram slider
+    function initInstaGlide() {
+        var glide = new Glide('.instagram-glide', {
+            type: 'carousel',
+            autoplay: 10000,
+            gap: 30,
+            perView: 4,
+            breakpoints: {
+                768: {
+                    perView: 2
+                },
+                1200: {
+                    perView: 3
+                }
+            }
+        })      
+    
+        // fixes cumulative layout shift
+        glide.on(['build.before'], function() {
+            if (glideInstaEl) {
+                glideInstaEl.classList.add('instagram--visible');
+            }
+            });
+
+            glide.mount()
+    }
+
+    function initEventGlide() {
+        var glide = new Glide('.glide-event', {
+            type: 'carousel',
+            gap: 30,
+            perView: 1,
+            peek: {
+                before: 313,
+                after: 313
             },
             breakpoints: {
                 768: {
@@ -48,32 +116,5 @@ export namespace slider {
     
               glide.mount()
     }
-
-    function initInstaGlide() {
-        var glide = new Glide('.instagram-glide', {
-            type: 'carousel',
-            autoplay: 10000,
-            gap: 30,
-            perView: 4,
-            breakpoints: {
-                768: {
-                    perView: 2
-                },
-                1200: {
-                    perView: 3
-                }
-            }
-        })      
-    
-        // fixes cumulative layout shift
-        glide.on(['build.before'], function() {
-            if (glideInstaEl) {
-                glideInstaEl.classList.add('instagram--visible');
-            }
-            });
-
-            glide.mount()
-    }
-    
 }
     
