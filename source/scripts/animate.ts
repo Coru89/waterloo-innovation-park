01e677e3cc113e5590b6c63568cb3afc8a3104d6
Coru89 <corey.james.keller@gmail.comcom>
2023-05-05 13:48:15 +0700

    adding animations

---
+++ source/scripts/animate.ts
@@ -0,0 +1,6 @@
+import AOS from 'aos';
+export namespace animate {
+    AOS.init({
+      offset: 120
+    });
+}
--- source/scripts/index.ts
+++ source/scripts/index.ts
@@ -4,7 +4,8 @@ import { Calendar } from './calendar';
 import { fancybox } from './fancybox';
 import {slider} from './slider';
 import { form } from './form';
+import { animate } from './animate'
 
 
 
-export { readmore, Nav, Calendar, fancybox, slider, form };
+export { readmore, Nav, Calendar, fancybox, slider, form, animate };
