a74a8fe00d1759c61fde8532e7a2f2216d652f87
Coru89 <corey.james.keller@gmail.comcom>
2023-07-28 16:53:54 +0700

    form

--- config/contact-form.php
+++ config/contact-form.php
@@ -3,11 +3,8 @@
 $config = [];
 $request = Craft::$app->request;
 
-if (
-    !$request->getIsConsoleRequest()
-    // ($toEmail = $request->getValidatedBodyParam('toEmail')) !== null
-) {
-    $config['toEmail'] = $toEmail;
-}
+
+$config['toEmail'] = $toEmail;
+
 
 return $config;
