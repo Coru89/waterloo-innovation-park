fe8ab5d2306e38ca4c4a6a7ec00289004696361e
Coru89 <corey.james.keller@gmail.comcom>
2023-07-28 17:43:04 +0700

    contact

--- config/contact-form.php
+++ config/contact-form.php
@@ -4,27 +4,9 @@ $config = [];
 $request = Craft::$app->request;
 
 // Get the 'toEmail' field from POST
-$toEmailField = $request->getBodyParam('toEmail');
+$toEmail = $request->getValidatedBodyParam('toEmail');
 // Check it's not a console request and the field is not null
-if ($toEmailField !== null && !$request->getIsConsoleRequest()) {   
-   // Hash the email provided
-   $toEmailField = Craft::$app->getSecurity()->hashData($toEmailField, null);
-   // Verify (may not be needed);
-   $isValid = Craft::$app->getSecurity()->validateData($toEmailField);
-}
-
-// If not a console request, the 'toEmail' field is not null and the field has ben validated
-if (
-      !$request->getIsConsoleRequest() &&
-      $toEmailField !== null && 
-      $isValid !== null
-   ) {
-      // set the 'toEmail' config to the new hashed email
-      $config['toEmail'] = $toEmailField;
-} elseif (
-      !$request->getIsConsoleRequest() &&
-      ($toEmail = $request->getValidatedBodyParam('toEmail')) !== null
-      ) {
+if ($toEmail !== null && !$request->getIsConsoleRequest()) {
    $config['toEmail'] = $toEmail;
 }
 
